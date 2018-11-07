45acc9df8d39e0008deb466c391e96b139ce8158
sigtot <sigurdtotland@gmail.com>
2018-11-07 14:47:14 +0100

    Implement circle detection

---
+++ main.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"gocv.io/x/gocv"
+	"image"
+	"image/color"
+	"log"
+)
+
+func main() {
+	webcam, _ := gocv.OpenVideoCapture(1)
+	defer webcam.Close()
+
+	window := gocv.NewWindow("Circles")
+	img := gocv.NewMat()
+
+	for {
+		showImg(webcam, window, img)
+	}
+}
+
+func showImg(webcam * gocv.VideoCapture, window * gocv.Window, img gocv.Mat) {
+	if ok := webcam.Read(&img); !ok {
+		log.Fatal("Webcam closed")
+	}
+
+	if img.Empty() {
+		log.Println("Warning: Read empty image")
+		return
+	}
+
+	gocv.MedianBlur(img, &img, 15)
+
+	cimg := gocv.NewMat()
+	defer cimg.Close()
+
+	gocv.CvtColor(img, &img, gocv.ColorRGBToGray)
+	gocv.CvtColor(img, &cimg, gocv.ColorGrayToBGR)
+
+	circles := gocv.NewMat()
+	defer circles.Close()
+
+	gocv.HoughCirclesWithParams(
+		img,
+		&circles,
+		gocv.HoughGradient,
+		1, // dp
+		float64(img.Rows()/8), // minDist
+		75, // param1
+		40, // param2
+		3, // minRadius
+		60,  // maxRadius
+	)
+
+	blue := color.RGBA{0, 0, 255, 0}
+	red := color.RGBA{255, 0, 0, 0}
+
+	for i := 0; i < circles.Cols(); i++ {
+		v := circles.GetVecfAt(0, i)
+		// if circles are found
+		if len(v) > 2 {
+			x := int(v[0])
+			y := int(v[1])
+			r := int(v[2])
+
+			gocv.Circle(&cimg, image.Pt(x, y), r, blue, 2)
+			gocv.Circle(&cimg, image.Pt(x, y), 2, red, 3)
+		}
+	}
+
+	window.IMShow(cimg)
+	window.WaitKey(1)
+}
