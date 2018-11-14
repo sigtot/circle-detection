5703979e3f235fd823d90fe4d2944cd3520c3eac
sigtot <sigurdtotland@gmail.com>
2018-11-14 03:52:58 +0100

    Possibly fix memory leak by removing defer statements from a for loop

--- main.go
+++ main.go
@@ -38,72 +38,75 @@ func main() {
 	for {
 		select {
 		case <-ticker.C:
-			img := gocv.NewMat()
-			if ok := webcam.Read(&img); !ok {
-				log.Fatal("Webcam closed")
-			}
-
-			if img.Empty() {
-				log.Println("Warning: Read empty image")
-				return
-			}
-
-			cimg := gocv.NewMat()
-			defer cimg.Close()
-
-			gocv.CvtColor(img, &img, gocv.ColorRGBToGray)
-			gocv.CvtColor(img, &cimg, gocv.ColorGrayToBGR)
-
-			circles := gocv.NewMat()
-			defer circles.Close()
-
-			gocv.HoughCirclesWithParams(
-				img,
-				&circles,
-				gocv.HoughGradient,
-				1, // dp
-				float64(img.Rows()/8), // minDist
-				75, // param1
-				25, // param2
-				25, // minRadius
-				28, // maxRadius
-			)
-
-			blue := color.RGBA{0, 0, 255, 0}
-			red := color.RGBA{255, 0, 0, 0}
-			redPred := color.RGBA{255, 0, 0, 150}
-
-			for i := 0; i < circles.Cols(); i++ {
-				v := circles.GetVecfAt(0, i)
-				// if circles are found
-				if len(v) > 2 {
-					x := int(v[0])
-					y := int(v[1])
-					r := int(v[2])
-
-					output := mat.NewVecDense(2, []float64{float64(x), float64(y)})
-					f.AddOutput(output)
-
-					gocv.Circle(&cimg, image.Pt(x, y), r, blue, 2)
-					gocv.Circle(&cimg, image.Pt(x, y), 2, red, 3)
-
-					// Draw 10 predicted positions
-					for i := 0; i < 100; i++ {
-						aPostStateEst := f.APostStateEst(f.CurrentK() + i)
-						predX := aPostStateEst.At(0, 0)
-						predY := aPostStateEst.At(1, 0)
-						gocv.Circle(&cimg, image.Pt(int(predX), int(predY)), 2, redPred, 2)
-					}
-				}
-			}
-
-			window.IMShow(cimg)
-			window.WaitKey(1)
+			drawPredictImage(window, webcam, &f)
 		case <-quit:
 			ticker.Stop()
 			return
 		}
 	}
+}
+
+func drawPredictImage(window *gocv.Window, webcam *gocv.VideoCapture, f *kalman.Filter) {
+	img := gocv.NewMat()
+	if ok := webcam.Read(&img); !ok {
+		log.Fatal("Webcam closed")
+	}
+
+	if img.Empty() {
+		log.Println("Warning: Read empty image")
+		return
+	}
+
+	cimg := gocv.NewMat()
+	defer cimg.Close()
+
+	gocv.CvtColor(img, &img, gocv.ColorRGBToGray)
+	gocv.CvtColor(img, &cimg, gocv.ColorGrayToBGR)
+
+	circles := gocv.NewMat()
+	defer circles.Close()
+
+	gocv.HoughCirclesWithParams(
+		img,
+		&circles,
+		gocv.HoughGradient,
+		1, // dp
+		float64(img.Rows()/8), // minDist
+		75, // param1
+		25, // param2
+		25, // minRadius
+		28, // maxRadius
+	)
+
+	blue := color.RGBA{0, 0, 255, 0}
+	red := color.RGBA{255, 0, 0, 0}
+	redPred := color.RGBA{255, 0, 0, 150}
+
+	for i := 0; i < circles.Cols(); i++ {
+		v := circles.GetVecfAt(0, i)
+		// if circles are found
+		if len(v) > 2 {
+			x := int(v[0])
+			y := int(v[1])
+			r := int(v[2])
+
+			output := mat.NewVecDense(2, []float64{float64(x), float64(y)})
+			f.AddOutput(output)
+
+			gocv.Circle(&cimg, image.Pt(x, y), r, blue, 2)
+			gocv.Circle(&cimg, image.Pt(x, y), 2, red, 3)
+
+			// Draw 10 predicted positions
+			for i := 0; i < 100; i++ {
+				aPostStateEst := f.APostStateEst(f.CurrentK() + i)
+				predX := aPostStateEst.At(0, 0)
+				predY := aPostStateEst.At(1, 0)
+				gocv.Circle(&cimg, image.Pt(int(predX), int(predY)), 2, redPred, 2)
+			}
+		}
+	}
+	window.IMShow(cimg)
+	window.WaitKey(1)
 }
 
 func printLocation(webcam *gocv.VideoCapture) {
